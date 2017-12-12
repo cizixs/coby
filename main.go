9b63116df0232f2ff2a2517c10f50372e1c12c70
wu.wei <wei.wu@transwarp.io>
2017-12-12 13:53:27 +0800

    Seperate init process
    In init process, before running user specified command,
    we can prepare container environtment, such as setup all
    mount points.

--- main.go
+++ main.go
@@ -1,14 +1,15 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"os"
 	"os/exec"
 	"syscall"
 )
 
-func main() {
-	cmd := exec.Command("sh")
+func run() {
+	cmd := exec.Command("/proc/self/exe", "init")
 	cmd.SysProcAttr = &syscall.SysProcAttr{
 		Cloneflags: syscall.CLONE_NEWUTS |
 			syscall.CLONE_NEWNS |
@@ -23,5 +24,50 @@ func main() {
 
 	if err := cmd.Run(); err != nil {
 		log.Fatal(err)
+	}
+}
+
+func initProcess() {
+	// initial setup, before running the real command
+	mountFlags := syscall.MS_NOEXEC | syscall.MS_NOSUID | syscall.MS_NODEV
+	if err := syscall.Mount("proc", "/proc", "proc", uintptr(mountFlags), ""); err != nil {
+		fmt.Printf("mount proc failed: %v", err)
+		return
+	}
+
+	if err := syscall.Sethostname([]byte("container")); err != nil {
+		fmt.Printf("set hostname failed: %v", err)
+	}
+
+	cmd := exec.Command("bash")
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	if err := cmd.Run(); err != nil {
+		fmt.Printf("run command in container failed: %v", err)
+		os.Exit(1)
+	}
+
+	// umount proc to make host work normally
+	if err := syscall.Unmount("/proc", 0); err != nil {
+		fmt.Printf("umount proc failed: %v", err)
+		os.Exit(1)
+	}
+}
+
+func main() {
+	if len(os.Args) < 2 {
+		fmt.Printf("Usage: %s run\n", os.Args[0])
+		return
+	}
+
+	switch os.Args[1] {
+	case "run":
+		run()
+	case "init":
+		initProcess()
+	default:
+		fmt.Printf("Unknown command: %s\n", os.Args[1])
 	}
 }
